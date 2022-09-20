37a313050311e50512ab1d848c53174e9dc844a5
Chia Yong Kang <chiayongkang@hotmail.com>
2022-09-20 13:33:11 +0800

    updated deploy script to include verification

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,6 +7,8 @@ async function main() {
   const VerifierContract = await ethers.getContractFactory("Verifier");
 	console.log("\nDeploying Verifier contract...");
 	const verifier = await VerifierContract.deploy();
+	await verifier.deployed();
+
 	console.log("Verifier contract deployed to:", verifier.address);
 
 	// Deploy zkSBT Contract
@@ -14,12 +16,12 @@ async function main() {
 	console.log("\nDeploying zkSBT contract...");
 	const zkSBT = await zkSBTContract.deploy("Spartan Labs ZK SBT", "zkSBT");
 	console.log("zkSBT contract deployed to:", zkSBT.address);
+	await zkSBT.deployed();
 
 
 	// wait for 1 block confirmation
 	console.log("\nWaiting for 1 block confirmation...");
-	await zkSBT.deployed();
-	await verifier.deployed();
+	await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 sec
 	
 	// Verify contract on Etherscan
 	console.log("\nVerifying zkSBT contract on Etherscan...");
@@ -28,12 +30,16 @@ async function main() {
 		contract: "contracts/zkSBT.sol:zkSBT",
 		constructorArguments: ["Spartan Labs ZK SBT", "zkSBT"],
 	});
-	console.log("zkSBT contract verified on Etherscan!");
+		console.log("Verifying Verifier contract on Etherscan...");
 	await hre.run("verify:verify", {
 		address: verifier.address,
 		contract: "contracts/Verifier.sol:Verifier",
 		constructorArguments: [],
 	});
+
+	console.log("Contracts verified on Etherscan!");
+	console.log("Address of zkSBT contract:", zkSBT.address);
+	console.log("Address of Verifier contract:", verifier.address);
 
 }
 

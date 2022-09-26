f1d3280712f1eb3deeeba4b933ddb41332c27515
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-09-26 14:55:20 +0800

    ignore build

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// next.config.js
+require("dotenv-flow").config({
+  path: ".config",
+  node_env: process.env.APP_ENV || process.env.NODE_ENV || "development",
+});
 
-module.exports = nextConfig;
+const env = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith("NEXT_PUBLIC_")) {
+    env[key] = process.env[key];
+  }
+});
+
+module.exports = {
+  env,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};

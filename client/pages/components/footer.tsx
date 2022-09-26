5394ce892ba773159e5693d29cdbee109b868512
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-09-26 14:50:33 +0800

    removed footer

--- client/pages/components/footer.tsx
+++
@@ -1,5 +0,0 @@
-      <footer className={styles.footer}>
-        <a href="https://spartanlabs.studio/" target="_blank" rel="noopener noreferrer">
-          Made with ❤️ by your frens at Spartan Labs
-        </a>
-      </footer>

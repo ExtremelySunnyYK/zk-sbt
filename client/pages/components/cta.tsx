54b6be61db843c6d806fc95d28d65d5c6706ef89
Chia Yong Kang <chiayongkang@hotmail.com>
2022-10-07 19:36:32 +0800

    styling fixes

--- client/pages/components/cta.tsx
+++ client/pages/components/cta.tsx
@@ -5,7 +5,8 @@ export default function CallToAction() {
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Interested in what we're building next or building with us?</span>
+          <span className="block">Interested in what we're building next</span>
+          <span className="block">Or Interested to build with us?</span>
           <span className="block text-indigo-600">Reach out to us today!</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
@@ -17,10 +18,10 @@ export default function CallToAction() {
               Get started
             </a>
           </div> */}
-          <div className="ml-3 inline-flex rounded-md shadow">
+          <div className="ml-6 inline-flex rounded-md shadow">
             <a
               href="https://twitter.com/TheSpartanLabs"
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              className="inline-flex items-center justify-center rounded-md border border-blue bg-white px-8 py-6 text-base font-medium text-indigo-600 hover:bg-indigo-50"
             >
               Follow Us!
             </a>

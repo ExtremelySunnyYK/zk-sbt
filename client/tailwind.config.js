3c0b55a5d6396ad32f43db269c795121bffcbdb5
Chia Yong Kang <chiayongkang@hotmail.com>
2022-10-07 14:26:43 +0800

    added darkmode protection

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-        // add this lines
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    // add this lines
+    './pages/**/*.{js,ts,jsx,tsx}',
     // if you have other folder, add its path too, like this:
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }

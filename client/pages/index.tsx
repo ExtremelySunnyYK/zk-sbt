971448465df83cd9d8f1ceed683eaeaabeb1072f
Chia Yong Kang <chiayongkang@hotmail.com>
2022-10-13 23:34:50 +0800

    fixed input label

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -293,7 +293,7 @@ const Home: NextPage = () => {
             <input id="credit_score" 
               type="text"
               className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-              placeholder='Input a positive integer as credit score'
+              placeholder='Input a credit score from 0 to 100'
               required
               value={getCreditScore} 
               onChange={handleCreditScoreChange} />

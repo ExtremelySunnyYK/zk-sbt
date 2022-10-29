e955fc013ff3241f5b32eabf9e97382e6ca5eaa7
Chia Yong Kang <chiayongkang@hotmail.com>
2022-10-29 20:04:45 +0800

    updatede api key

--- client/globals/urlConfig.ts
+++ client/globals/urlConfig.ts
@@ -1,5 +1,6 @@
 // const baseUrl = `${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}/api`;
 const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
+// const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
 export const generateProofUrl = `${baseUrl}/generate-proof`;
 export const generateCallDataUrl = `${baseUrl}/generate-call-data`;
 export const verifySbtUrl = `${baseUrl}/verify-sbt`;
